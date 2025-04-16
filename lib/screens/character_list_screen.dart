7e1fcd231b270c4b14cc15ac645635596354e3da
Remi de Boer <remideboer@yahoo.com>
2025-04-17 00:46:53 +0200

    skull is correctly showing on dead characters

--- lib/screens/character_list_screen.dart
+++ lib/screens/character_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../models/character.dart';
 import '../repositories/local_character_repository.dart';
 import '../theme/app_theme.dart';
@@ -44,12 +45,33 @@ class _CharacterListScreenState extends State<CharacterListScreen> {
         itemCount: _characters.length,
         itemBuilder: (context, index) {
           final character = _characters[index];
+          final isDead = character.lifeStat.current == 0;
+          
           return Card(
             margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: ListTile(
-              title: Text(
-                character.name,
-                style: AppTheme.titleStyle,
+              title: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      character.name,
+                      style: AppTheme.titleStyle,
+                    ),
+                  ),
+                  if (isDead)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: SvgPicture.asset(
+                        'assets/svg/death-skull.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: const ColorFilter.mode(
+                          Colors.black,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                ],
               ),
               subtitle: Text(
                 character.species.name,
